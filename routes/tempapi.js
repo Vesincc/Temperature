1962bef20e5fc97980284011388d2cade12fd4f9
HanQi <1464467970@qq.com>
2016-05-28 21:14:22 +0800

    limit

--- routes/tempapi.js
+++ routes/tempapi.js
@@ -87,6 +87,7 @@ router.get('/getinfo', function(req, res, next) {
         query.greaterThan('updatedAt', new Date(date + ' 00:00:00'));
         query.lessThan('updatedAt', new Date(date + ' 23:59:59'));
     }
+    query.limit(9999);
     query.find().then(function (resultes) {
         console.log();
         for (var i = 0 ; i < resultes.length; i++) {
